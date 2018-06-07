f2e0d4a61e7e4d0938c36655a4e0358a443ce6d9
Unknown <yeraymendez07@gmail.com>
2018-06-07 20:01:24 +0200

    Fai maxia

--- WebApplication/Pages/Card/SeeMyCards.aspx
+++ WebApplication/Pages/Card/SeeMyCards.aspx
@@ -11,14 +11,19 @@
             AutoGenerateColumns="False"
             OnSelectedIndexChanging="gvAllCards_SelectedIndexChanging"
             OnRowCreated="gvAllCards_RowCreated"
+            OnRowDataBound="gvAllCards_RowDataBound"
             ShowHeaderWhenEmpty="True" meta:resourcekey="gvAllCardsResource1">
             <Columns>
             <asp:BoundField DataField="CardNumber"  HeaderText="<%$ Resources:, CardNumber %>" meta:resourcekey="BoundFieldResource1" />
             <asp:BoundField DataField="CardType" HeaderText="<%$ Resources:, CardType %>" meta:resourcekey="BoundFieldResource2" />
             <asp:BoundField DataField="ExpirateTime" HeaderText="<%$ Resources:, ExpirateTime %>" meta:resourcekey="BoundFieldResource3" />
             <asp:BoundField DataField="CardId" HeaderText="<%$ Resources:, CardId %>" Visible="true" meta:resourcekey="BoundFieldResource4"  />
-            <asp:CheckBoxField DataField="DefaultCard" HeaderText="<%$ Resources:, DefaultCard %>" Visible="true" meta:resourcekey="BoundFieldResource5"  />
-            <asp:CommandField ShowSelectButton="True" SelectText="<%$ Resources:, changeDefault %>" meta:resourcekey="BoundFieldResource5" /> 
+            <asp:BoundField DataField="DefaultCard" HeaderText="<%$ Resources:, DefaultCard %>" Visible="true" meta:resourcekey="BoundFieldResource4"  />
+                <asp:TemplateField  HeaderText="<%$ Resources:, DefaultCard %>">
+                    <ItemTemplate>
+                        <asp:checkbox ID="changeDefaultCard" AutoPostBack="true" runat="server" OnCheckedChanged="changeDefaultCard_CheckedChanged" />
+                    </ItemTemplate>
+                </asp:TemplateField>
         </Columns>
         </asp:GridView>
     </form>
--- WebApplication/Pages/Card/SeeMyCards.aspx.cs
+++ WebApplication/Pages/Card/SeeMyCards.aspx.cs
@@ -22,6 +22,7 @@ namespace Es.Udc.DotNet.PracticaMaD.WebApplication.Pages.Card
         protected void gvAllCards_RowCreated(object sender, System.Web.UI.WebControls.GridViewRowEventArgs e)
         {
             e.Row.Cells[3].Visible = false;
+            e.Row.Cells[4].Visible = false;
         }
 
         protected void gvAllCards_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
@@ -34,6 +35,27 @@ namespace Es.Udc.DotNet.PracticaMaD.WebApplication.Pages.Card
             Response.Redirect(Request.RawUrl.ToString());
             gvAllCards.DataSource = cardService.ViewCardsByUser(usrId);
             gvAllCards.DataBind();
+        }
+
+        protected void changeDefaultCard_CheckedChanged(object sender, EventArgs e)
+        {
+            /*CheckBox checkBox = sender as CheckBox;
+            GridViewRow row = checkBox.NamingContainer as GridViewRow;
+            long cardId = (long)Convert.ToInt32(row.Cells[3].Text);
+            long usrId = SessionManager.GetUserSession(Context).UserProfileId;
+            IIoCManager iocManager = (IIoCManager)HttpContext.Current.Application["managerIoC"];
+            ICardService cardService = (ICardService)iocManager.Resolve<ICardService>();
+            cardService.ChangeDefaultCard(usrId, cardId);
+            Response.Redirect(Request.RawUrl.ToString());*/
+            Response.Redirect(Response.
+                        ApplyAppPathModifier("~/Pages/MainPage.aspx"));
+        }
+
+        protected void gvAllCards_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            var chB = e.Row.Cells[5].FindControl("changeDefaultCard") as CheckBox;
+            if (e.Row.Cells[4].Text.Equals("True") && chB != null)
+                chB.Checked = true;
         }
     }
 }
